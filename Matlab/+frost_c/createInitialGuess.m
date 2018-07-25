93c1759a2e741ed4e88b26ddf664a8a2b5888673
Omar Harib <omar.harib@outlook.com>
2018-07-25 14:34:45 -0400

    allow custom init name files

--- Matlab/+frost_c/createInitialGuess.m
+++ Matlab/+frost_c/createInitialGuess.m
@@ -1,6 +1,10 @@
-function createInitialGuess(solver, data_path, x0)
-    if ~exist('x0', 'var')
+function createInitialGuess(solver, data_path, x0, name)
+    if ~exist('x0', 'var') || isempty(x0)
         x0 = getInitialGuess(solver.Nlp, solver.Options.initialguess);
+    end
+    
+    if ~exist('name', 'var')
+        name = 'init';
     end
     
     if iscolumn(x0)
@@ -12,5 +16,5 @@ function createInitialGuess(solver, data_path, x0)
     end
     
     % Uses RapidJSON
-    savejson('', x0, fullfile(data_path,'init.json'));
+    savejson('', x0, fullfile(data_path, [name, '.json']));
 end
